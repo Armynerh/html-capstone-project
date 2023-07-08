350b743aba5be5a3930e8166355c943e92159f29
Armynerh <aimenah@outlook.com>
2023-07-08 07:20:00 +0100

    Modify js file to implement review changes

--- app.js
+++ app.js
@@ -16,50 +16,50 @@ document.querySelectorAll('.nav-item').forEach((n) => n.addEventListener('click'
 const speakers = [
   {
     name: 'Jason Butler',
-    description: 'simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer',
+    description: 'simply dummy text of the printing and typesetting industry and has been the industry\'s ever since the 1500s, when an unknown printer',
     image: 'images/close-up-portrait-young-bearded-man-white-shirt-jacket-posing-camera-with-broad-smile-isolated-gray.jpeg',
-    role: 'Head of Programmes',
+    role: 'Head of Programmes Lead Faci in charge ',
   },
 
   {
     name: 'Brandel Smith',
-    description: 'simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer',
+    description: 'simply dummy text of the printing and typesetting industry and has been the industry\'s  ever since the 1500s, when an unknown printer',
     image: 'images/african-woman-successful-entrepreneur-wearing-glasses-face-portrait.jpeg',
-    role: 'curriculum Developer',
+    role: 'curriculum Developer Lead Faci in charge ',
   },
   {
     name: 'Tade Johnson',
-    description: 'simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer',
+    description: 'simply dummy text of the printing and typesetting industry and has been the industry\'s  ever since the 1500s, when an unknown printer',
     image: 'images/confident-african-businesswoman-smiling-closeup-portrait-jobs-career-campaign.jpeg.crdownload',
-    role: 'Instructor',
+    role: 'Instructor Lead Faci in charge ',
   },
   {
     name: 'Michael Brian',
-    description: 'simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer',
+    description: 'simply dummy text of the printing and typesetting industry and has been the industry\'s  ever since the 1500s, when an unknown printer',
     image: 'images/happy-successful-businessman-posing-outside.jpeg',
-    role: 'Edtech Coach',
+    role: 'Edtech Coach Lead Faci in ',
   },
   {
     name: 'Ariel Tamad-Lee',
-    description: 'simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer.',
+    description: 'simply dummy text of the printing and typesetting industry and has been the industry\'s ever since the 1500s, when an unknown printer.',
     image: 'images/confident-african-businesswoman-smiling-closeup-portrait-jobs-career-campaign.jpeg.crdownload',
-    role: 'Student Team',
+    role: 'Student Team Lead Faci in charge ',
   },
   {
     name: 'Amie Sanderson',
-    description: 'simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer.',
+    description: 'simply dummy text of the printing and typesetting industry and has been the industry\'s  ever since the 1500s, when an unknown printer.',
     image: 'images/confident-professional-businesswoman-cross-arms-chest-looking-selfassured-camera-smiling-standing-white-background.jpeg',
-    role: 'Lead Faci',
+    role: 'Lead Faci in charge of everything',
   },
 ];
 const works = document.querySelector('.team');
 const newWorks = speakers.map((speaker) => `<div class="grid person">
     <div class="para-div flex">
         <img src=${speaker.image}
-         class="img2">
-         <div class='para'><p style="font-size:12px">${speaker.name}</p>
-    <p style="font-size:12px; color= #ec4327">${speaker.role}</p>
-     <p style="font-size:5px">${speaker.description}</p></div>
+         class="img5">
+         <div class='para'><p class= 'speaker-head'>${speaker.name}</p>
+    <p class="desc1">${speaker.role}</p>
+     <p class="desc2">${speaker.description}</p></div>
     </div>
     
     </div>`).join('');
